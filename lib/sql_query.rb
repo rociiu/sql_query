b1a5a6aeae2ee35f8819279b0d6d957a9fec65f2
rociiu <rociiu.yu@gmail.com>
2009-08-31 10:56:20 +0800

    init project

---
+++ lib/sql_query.rb
@@ -0,0 +1,30 @@
+module SqlQuery
+  
+  def self.included(base)
+    base.extend(SqlQuery::ClassMethods)
+  end
+  
+  module ClassMethods
+    
+    def sql_query(options={})
+      if options[:fields].nil?
+        options[:fields] = []
+        self.columns.each do |column|
+          options[:fields] << column.name.to_s if [:string, :text].include?(column.type)
+        end
+      end
+      cons_array = []
+      options[:fields].each do |field|
+        cons_array << " #{field} like :query"
+      end
+      cons = cons_array.join(' or ')
+      named_scope :sql_search, lambda { |query|
+        h = {}
+        h[:query] = query
+        { :conditions => [cons, h]}
+      }
+    end
+    
+  end
+  
+end
---
+++ tasks/sql_query_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :sql_query do
+#   # Task goes here
+# end
